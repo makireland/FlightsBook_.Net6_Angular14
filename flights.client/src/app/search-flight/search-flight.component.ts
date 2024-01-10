6be240b2d23a0b6ce8a366c60f027ca54faaf1c9
Maksuel Milhomem <mmilhomem@brainworks.com>
2024-01-10 00:56:55 +0000

    implementing client api and showing data coming from server

--- flights.client/src/app/search-flight/search-flight.component.ts
+++ flights.client/src/app/search-flight/search-flight.component.ts
@@ -1,54 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { FlightService } from './../api/services/flight.service';
+import { FlightRm } from '../api/models';
 
 @Component({
   selector: 'app-search-flight',
   templateUrl: './search-flight.component.html',
   styleUrls: ['./search-flight.component.css']
 })
+
 export class SearchFlightComponent implements OnInit {
 
   searchResult: FlightRm[] = [
-    {
-      airline: "TAM",
-      remainingNumberOfSeats: 500,
-      departure: { time: Date.now().toString(), place: "Los Angeles" },
-      arrival: { time: Date.now().toString(), place: "Instabul" },
-      price: "350",
-    },
-    {
-      airline: "Luftansa",
-      remainingNumberOfSeats: 230,
-      departure: { time: Date.now().toString(), place: "Mexico" },
-      arrival: { time: Date.now().toString(), place: "USA" },
-      price: "630",
-    },
-    {
-      airline: "Ryanair",
-      remainingNumberOfSeats: 75,
-      departure: { time: Date.now().toString(), place: "Dublin" },
-      arrival: { time: Date.now().toString(), place: "London" },
-      price: "120",
-    },
-    
+   
   ]
-  constructor() { }
+  constructor(private flightService: FlightService) { }
 
   ngOnInit(): void {
   }
 
-}
+  search()
+  {
+    this.flightService.flightGet({})
+      .subscribe(response => this.searchResult = response)
+  }
 
-export interface FlightRm
-{
-  airline: string;
-  arrival: TimePlaceRm;
-  departure: TimePlaceRm;
-  price: string;
-  remainingNumberOfSeats: number;
-}
-
-export interface TimePlaceRm
-{
-  place: string;
-  time: string;
 }
