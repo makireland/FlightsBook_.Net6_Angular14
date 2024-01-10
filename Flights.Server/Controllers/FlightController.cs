f467a731304fa10de058bdf51ccd462045a653ab
Maksuel Milhomem <mmilhomem@brainworks.com>
2024-01-10 16:43:28 +0000

    documenting swagger

--- Flights.Server/Controllers/FlightController.cs
+++ Flights.Server/Controllers/FlightController.cs
@@ -18,44 +18,56 @@ namespace Flights.Server.Controllers
         static private FlightRm[] flights = new FlightRm[]
         {
             new (Guid.NewGuid(),
-                "America Airline",
-                random.Next(90,5000).ToString(),
-                new TimePlaceRm("Los Angeles", DateTime.Now.AddHours(random.Next(1, 3))),
-                new TimePlaceRm("Istanbul", DateTime.Now.AddHours(random.Next(4, 13))),
-                random.Next(1, 853)),
+            "America Airline",
+            random.Next(90,5000).ToString(),
+            new TimePlaceRm("Los Angeles", DateTime.Now.AddHours(random.Next(1, 3))),
+            new TimePlaceRm("Istanbul", DateTime.Now.AddHours(random.Next(4, 13))),
+            random.Next(1, 853)),
 
-                new (Guid.NewGuid(),
-                "Airlingus",
-                random.Next(90,5000).ToString(),
-                new TimePlaceRm("Dublin", DateTime.Now.AddHours(random.Next(1, 13))),
-                new TimePlaceRm("Spain", DateTime.Now.AddHours(random.Next(4, 15))),
-                random.Next(1, 853)),
+            new (Guid.NewGuid(),
+            "Airlingus",
+            random.Next(90,5000).ToString(),
+            new TimePlaceRm("Dublin", DateTime.Now.AddHours(random.Next(1, 13))),
+            new TimePlaceRm("Spain", DateTime.Now.AddHours(random.Next(4, 15))),
+            random.Next(1, 853)),
 
-                new (Guid.NewGuid(),
-                "Ryanair",
-                random.Next(90,5000).ToString(),
-                new TimePlaceRm("London", DateTime.Now.AddHours(random.Next(1, 15))),
-                new TimePlaceRm("Portugal", DateTime.Now.AddHours(random.Next(4, 18))),
-                random.Next(1, 853)),
+            new (Guid.NewGuid(),
+            "Ryanair",
+            random.Next(90,5000).ToString(),
+            new TimePlaceRm("London", DateTime.Now.AddHours(random.Next(1, 15))),
+            new TimePlaceRm("Portugal", DateTime.Now.AddHours(random.Next(4, 18))),
+            random.Next(1, 853)),
 
-                new (Guid.NewGuid(),
-                "Latam",
-                random.Next(90,5000).ToString(),
-                new TimePlaceRm("Sao Paulo", DateTime.Now.AddHours(random.Next(1, 12))),
-                new TimePlaceRm("Goiania", DateTime.Now.AddHours(random.Next(2, 16))),
-                random.Next(1, 853)),
+            new (Guid.NewGuid(),
+            "Latam",
+            random.Next(90,5000).ToString(),
+            new TimePlaceRm("Sao Paulo", DateTime.Now.AddHours(random.Next(1, 12))),
+            new TimePlaceRm("Goiania", DateTime.Now.AddHours(random.Next(2, 16))),
+            random.Next(1, 853)),
         };
 
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [ProducesResponseType(typeof(IEnumerable<FlightRm>), StatusCodes.Status200OK)]
         [HttpGet]
         public IEnumerable<FlightRm> Search()
         {
             return flights;
         }
 
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [ProducesResponseType(typeof(FlightRm), StatusCodes.Status200OK)]
         [HttpGet("{id}")]
-        public FlightRm Find(Guid id)
+        public ActionResult<FlightRm> Find(Guid id)
         {
-            return flights.SingleOrDefault(f => f.Id == id);
+            var flight = flights.SingleOrDefault(f => f.Id == id);
+            
+            if (flight == null)
+                return NotFound();
+
+            return Ok(flight);
         }
     }
 }
